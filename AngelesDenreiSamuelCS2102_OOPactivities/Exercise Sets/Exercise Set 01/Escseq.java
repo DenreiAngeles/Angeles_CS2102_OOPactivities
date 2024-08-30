5139e978446456d6dc8070cef531fd92fb9b4ca6
DenreiAngeles <163831037+DenreiAngeles@users.noreply.github.com>
2024-08-30 14:25:16 +0800

    Add files via upload

---
+++ AngelesDenreiSamuelCS2102_OOPactivities/Exercise Sets/Exercise Set 01/Escseq.java
@@ -0,0 +1,12 @@
+public class Escseq {
+    public static void main(String[] args) {
+        for (int i = 1; i < 6; i++) {
+            if(i % 2 == 0) {
+                System.out.println("\t" + (i*7));
+            } 
+            else {
+                System.out.println(i*7);
+            }
+        }
+    }
+}
